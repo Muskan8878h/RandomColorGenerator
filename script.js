87a1cc314762b6f077e183ee942dff9fa401a9f5
muskan <muskan8878h@gmail.com>
2024-10-06 12:09:23 +0530

    a function created inside function we calculate the hex value for color code

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const getColor=()=>{
+    const randomNum=Math.floor(Math.random()*16777215);  // multiple with num to make it hexa decimal number 
+    const randomCode="#"+randomNum.toString(16);
+    // console.log(randomNum,randomColor);
 
+    
+}
